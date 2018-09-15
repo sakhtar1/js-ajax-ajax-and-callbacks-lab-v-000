e33e811b06415bec2e959bf3998a7739418b6ec5
Sharmina Akhtar <sakht318@gmail.com>
2018-09-15 11:40:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ var searchRepositories = () => {
 
 
 var renderSearchResults = (results) => {
-  const resultList = results.items.map(result => {
-    return '<div><h4><a href="test">${result.name}</a></h4> <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p><p>${result.description}</p></div>'
+  const resultList = results.map(result => {
+    '<div><h4><a href="test">' + result.name + '</a></h4> <p><a href="#" data-repository="' +result.name + 'data-owner=' + result.owner.login + 'onclick="showCommits(this)">Show Commits</a></p><p>'result.description+ '</p></div>'
   });
   return resultList
 };
